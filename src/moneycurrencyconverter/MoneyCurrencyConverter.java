efc3c96c969599277ede53ef027e29a577b48f57
GUESTses <GUESTses@gabHalfOutrovertHalfInverter>
2025-05-25 21:07:53 +0800

    revert

---
+++ src/moneycurrencyconverter/MoneyCurrencyConverter.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moneycurrencyconverter;
+
+/**
+ *
+ * @author kearl
+ */
+public class MoneyCurrencyConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test one");
+        
+        System.out.println("test two");
+        // TODO code application logic here
+    }
+    
+}
